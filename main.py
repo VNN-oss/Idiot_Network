19d7cd6002464d776ae5bbf49b340cf17ad6335f
VNN-oss <70770522+VNN-oss@users.noreply.github.com>
2020-09-27 09:55:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-from neural_network import Neural_Network
+from neural_network import Nerual_Network
+import psudeo_generator import Generate
+Net0 = Nerual_Network(2500, .625)
 Net1 = Nerual_Network(2000, .2)
+Net2 = Nerual_Network(1500, 0.0375)
+Net3 = Nerual_Network(1000, 0.025)
+Net4 = Nerual_Network(500, 0.0125)
+prob = Generate(2500)
+Net0.create(prob)
