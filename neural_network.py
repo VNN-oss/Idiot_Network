1aad000de0eb5685bb80ebf278364300973eb5e0
VNN-oss <70770522+VNN-oss@users.noreply.github.com>
2020-09-27 16:25:30 -0500

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -1,4 +1,4 @@
-
+#Full nerual_network class
 import pandas as pd
 import seaborn as sns
 import random as r
@@ -9,28 +9,34 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 #from pesudo_generator import generator
 class Nerual_Network():
-    def __init__(self, data_amount,t_size):
+    def __init__(self, data_amount,t_size,y_test,pred):
         self.data_amount = data_amount
         self.t_size = t_size
-    def create(self,math_problems):
-
+        self.y_test = y_test
+        self.pred = pred
+    def create(self,data):
         print("Creating dataframe...")
-        math_problems = pd.DataFrame(math_problems)
-        sns.countplot(math_problems['Distance'])
-        X = math_problems.drop("Distance", axis = 1) 
-        y = math_problems['Distance']#Actual answer
+        data = pd.DataFrame(data)
+        X = data.drop("Distance", axis = 1) 
+        y = data['Distance']#Actual answer
         
         print("Spliting data")
         #Split data
         X_train, X_Test, y_train,y_test = train_test_split(X,y, test_size = self.t_size, random_state = 50)
+        
         print("Creating network")
         #Create Neural Network
-        mlpc = MLPClassifier(hidden_layer_sizes = (11,11,11), max_iter = 500)
-        print("Training network")
-        mlpc.fit(X_train,y_train)
-        pred = mlpc.predict(X_Test)
-        print("Network complete!")
+        mlpc = MLPClassifier(hidden_layer_sizes = (12,12,12), max_iter = 520)
         
+        print("Training network....")
+        mlpc.fit(X_train,y_train)
+
+        pred = mlpc.predict(X_Test)
+
+        print("Creating report")
         #Return and plot accuracy
         print(classification_report(y_test,pred))
-        plot = sns.jointplot(x = "Correct", y = "Incorrect", data = y_test,kind = "reg", truncate = False, xlim = (0, self.data_amount), ylim = (0, self.t_size), color = 'r', height = 7)
+        print("Creating confusion matrix")
+        print(confusion_matrix(y_test, pred))
+        #plot = sns.jointplot(x = "Correct", y = "Incorrect", data = self.y_test,kind = "reg", truncate = False, xlim = (0, self.data_amount), ylim = (0, self.t_size), color = 'r', height = 7)
+        return self.pred
