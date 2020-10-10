800877b1afee51cc907b5a17961b04efff481e67
VNN-oss <70770522+VNN-oss@users.noreply.github.com>
2020-10-10 17:17:41 -0500

    Fit psudeo_generator in class structure.

--- psudeo_generator.py
+++ psudeo_generator.py
@@ -1,41 +1,47 @@
-def Generate(runtime):
-    #Initailze varibles
-    math = {"Questions":[], "Answer": [], "Distance": []}
-    clock = r.randint(0,1)
-    
-    #Create questions
-    for i in range(0, runtime):
-        #print(clock)
-        add1 = r.randint(1, r.randint(1, 100))
-        add2 = r.randint(1, r.randint(1, 100))
-        equ = r.randint(0, 2)
-        if (equ == 0):
-            ans = add1 + add2
-            que = str(add1) + str(add2) + '.1'
-            que = float(que)
-        elif(equ == 1):
-            ans = add1 * add2
-            que = str(add1) + str(add2) + '.2'
-            que = float(que)
-        elif(equ == 2):
-            ans = add1 - add2
-            que = str(add1) + str(add2) + '.3'
-            que = float(que)
-        else:
-            if (add1 < add2): 
-                add1 = add2
-                add2 = add1
-            ans = add1/add2
-        math["Questions"].append(que)
+import random as r
+import seaborn as sns
+class Set():
+    math = {"Questions":[], "Answer": []}
         
-        #Decide if question is right or wrong.
-        if (clock == 0):
-            clock = clock + 1
-            math["Answer"].append(ans)
-            math["Distance"].append(1)
-        else:
-            clock = clock -1
-            math["Answer"].append(r.randint(0, r.randint(1, 100)))
-            math["Distance"].append(0)
-    sns.countplot(math['Distance'])
-    return math
+    def Generate(self,runtime):
+        #Initailze local varibles
+        clock = r.randint(0,1)
+        que = 0
+        #Create questions
+        for i in range(0, runtime):
+            #print(clock)
+            add1 = r.randint(1, r.randint(1, 1000))
+            add2 = r.randint(1, r.randint(1, 1000))
+            equ = r.randint(0, 3)
+            if (equ == 0):
+                ans = add1 + add2
+                que = str(add1) + str(add2) + '.1'
+                que = float(que)
+            elif(equ == 1):
+                ans = add1 * add2
+                que = str(add1) + str(add2) + '.2'
+                que = float(que)
+            elif(equ == 2):
+                ans = add1 - add2
+                que = str(add1) + str(add2) + '.3'
+                que = float(que)
+            else:
+                if (add1 < add2):
+                    add1 = add2
+                    add2 = add1
+                ans = add1/add2
+            self.math["Questions"].append(que)
+        
+            #Decide if question is right or wrong.
+            if (clock == 0):
+                clock = clock + 1
+                self.math["Answer"].append(ans)
+            else:
+                clock = clock -1
+                self.math["Answer"].append(r.randint(0, r.randint(1, 100)))
+        sns.countplot(self.math['Distance'])
+        return self.math
+    def Cut (self,amount):
+        del self.math["Questions"][:amount]
+        del self.math["Answer"][:amount]
+        return self.math
