c56b89c308c8c2cff75fc88f35274a11dbbd9fde
VNN-oss <70770522+VNN-oss@users.noreply.github.com>
2020-10-14 10:28:40 -0500

    Fixed issues regarding labels and cutting function

--- psudeo_generator.py
+++ psudeo_generator.py
@@ -1,9 +1,9 @@
 import random as r
 import seaborn as sns
+import pandas as pd
 class Set():
-    math = {"Questions":[], "Answer": []}
-        
-    def Generate(self,runtime):
+    math = {"Questions":[], "Answer": [], "Distance":[]}
+    def generate(self,runtime):
         #Initailze local varibles
         clock = r.randint(0,1)
         que = 0
@@ -36,12 +36,19 @@ class Set():
             if (clock == 0):
                 clock = clock + 1
                 self.math["Answer"].append(ans)
+                self.math["Distance"].append(1)
             else:
                 clock = clock -1
-                self.math["Answer"].append(r.randint(0, r.randint(1, 100)))
-        sns.countplot(self.math['Distance'])
+                self.math["Answer"].append(r.randint(0, r.randint(1, 1000)))
+                self.math["Distance"].append(0)
+        sns.countplot(self.math["Distance"])
+        print("Qlen: {a}".format(a = len(self.math["Questions"])))
+        print("Alen: {a}".format(a = len(self.math["Answer"])))
+        print("Dlen: {a}".format(a = len(self.math["Distance"])))
         return self.math
     def Cut (self,amount):
-        del self.math["Questions"][:amount]
-        del self.math["Answer"][:amount]
+        del self.math["Questions"][amount:]
+        del self.math["Answer"][amount:]
+        del self.math["Distance"][amount:]
+        print("Data succesfully cut by {a}".f(a= amount))
         return self.math
